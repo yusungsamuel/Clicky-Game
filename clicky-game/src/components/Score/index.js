176c90f7a3252aa8e0fc22378d24746763055bed
yusungsamuel <ssyyu@ucdavis.edu>
2019-04-24 10:01:45 -0700

    create card component

--- clicky-game/src/components/Score/index.js
+++ clicky-game/src/components/Score/index.js
@@ -6,6 +6,7 @@ class Score extends Component {
         topScore:0,
     }
 
+
 }
 
 
---
+++ clicky-game/src/components/cards/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Card(){
+    return (
+        <div class="row">
+        <div class="col s12 m5">
+          <div class="card-panel teal">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"/>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Card;

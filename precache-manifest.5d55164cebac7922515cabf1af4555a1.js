e8179d52edb2622515bd53fe4345ede176c9228d
yusungsamuel <ssyyu@ucdavis.edu>
2019-04-26 10:02:50 -0700

    Updates

---
+++ precache-manifest.5d55164cebac7922515cabf1af4555a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "968edae0c1cf3f31debd45dc5fd9a33b",
+    "url": "/game-of-thrones-memory-game/index.html"
+  },
+  {
+    "revision": "34532723b3f805b34f50",
+    "url": "/game-of-thrones-memory-game/static/css/main.12680794.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/game-of-thrones-memory-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "34532723b3f805b34f50",
+    "url": "/game-of-thrones-memory-game/static/js/main.9a59db1a.chunk.js"
+  },
+  {
+    "revision": "e53b373a8bef53e08091",
+    "url": "/game-of-thrones-memory-game/static/js/runtime~main.08753155.js"
+  }
+]);
